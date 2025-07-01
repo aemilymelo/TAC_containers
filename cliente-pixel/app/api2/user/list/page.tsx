88d1312d092ee73a2cfe1880536eb7bdb7022fda
Andrade kaique <kaique.andrade@consulting-for.edenred.com>
2025-06-30 22:46:17 -0300

    api2

--- cliente-pixel/app/api2/user/list/page.tsx
+++ cliente-pixel/app/api2/user/list/page.tsx
@@ -1,13 +1,32 @@
 
 'use client';
-import Table from "@/app/component/table/TableView";
+
+
+import TableView from "@/app/component/table/TableView";
 import { useServiceV2 } from "@/app/v2/service/user";
+import { Alert, Snackbar } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const UserListPage: React.FC = () => {
     const service = useServiceV2();
    const [list, setList] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
+
+
+      const [message, setMessage] = useState({ message: "", status: "" });
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    console.log("openModal");
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setTimeout(() => {
+      setMessage({ message: "", status: "" });
+    }, 3000);
+    setOpen(false);
+  };
+
+
 
     useEffect(() => {
         service.findAll2()
@@ -22,13 +41,35 @@ const UserListPage: React.FC = () => {
 
     return (
     <div className=" m-10 p-20 w-full" >
-        <Table 
-        list={list} 
+
+
+              <TableView
+              removeItem={(id)=> service.remove(id.id).then(()=>{
+      setMessage({ message: "Registro remvido ", status: "sucesso" });
+        }).catch(error=>{
+      setMessage({ message: "Ops, alguma falha no sistema", status: "error" });
+        })}
+        openModal={handleOpen}
+        title="Pessoa"
+        list={list}
         columns={[
-            { id: 'id', label: 'ID' },
-            { id: 'nome', label: 'Nome' },
-            { id: 'email', label: 'Email' },
-        ]} />
+          { property: "id", label: "ID" },
+          { property: "nome", label: "Nome" },
+          { property: "tipo", label: "Tipo" },
+          { property: "tipo", label: "Tipo" },
+          { type: "action", label: "Remover" },
+        ]}
+      />
+        <Snackbar open={message.message != ""}>
+              <Alert
+                onClose={handleClose}
+                severity="success"
+                variant="filled"
+                sx={{ width: "100%" }}
+              >
+                {message.message}
+              </Alert>
+            </Snackbar>
     </div>
     );
 };
